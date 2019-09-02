879a52e3b1c7248ee9f7848bc858c18fb9677356
jyunki <jyunki9780@gmail.com>
2019-09-02 12:31:28 +0000

    test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,15 +1,15 @@
-require 'test_helper'
+# require 'test_helper'
 
-class SiteLayoutTest < ActionDispatch::IntegrationTest
- test "layout links" do
-    get root_path
-    assert_template 'pages/index'
-    assert_select "a[href=?]", root_path
-    assert_select "a[href=?]", kinds_index_path
-    assert_select "a[href=?]", favorites_index_path
-    assert_select "a[href=?]", users_edit_path
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", users_new_path
-    assert_select "a[href=?]", login_path
-  end
-end
+# class SiteLayoutTest < ActionDispatch::IntegrationTest
+#  test "layout links" do
+#     get root_path
+#     assert_template 'pages/index'
+#     assert_select "a[href=?]", root_path
+#     assert_select "a[href=?]", kinds_index_path
+#     assert_select "a[href=?]", favorites_index_path
+#     assert_select "a[href=?]", users_edit_path
+#     assert_select "a[href=?]", logout_path
+#     assert_select "a[href=?]", users_new_path
+#     assert_select "a[href=?]", login_path
+#   end
+# end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,12 +1,27 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  get users_new_path
-  assert_no_difference 'User.count' do
-  post users_new_path, params: { user: { name:  "",
-                                     email: "user@invalid",
-                                     password:              "foo",
-                                     password_confirmation: "bar" } }
+  
+  test "invalid signup information" do
+    get new_user_path
+    assert_no_difference 'User.count' do
+     post users_path, params: { user: { name:  "",
+                                      email: "user@invalid",
+                                      password:              "foo",
+                                      password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
   end
-  assert_template 'users/new'
+  
+  test "valid signup information" do
+    get new_user_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password111",
+                                         password_confirmation: "password111" } }
+    end
+    follow_redirect!
+    assert_template 'pages/index'
+  end
 end
